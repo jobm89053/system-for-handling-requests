56bd1c1b161de2251f3b0d1e6897a9a6b2e70501
jobm89053 <jobm89053@gmail.com>
2025-03-01 20:29:54 +0300

    commit 4

--- app/routes/all_appeal.js
+++ app/routes/all_appeal.js
@@ -6,17 +6,48 @@ const CONFIG =require ('../db/config');
 const Q =require('../db/queries')
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  
+router.get('/all_appeal', async function(req, res, next) {
+  let connection; 
 //работа с бд
+try {
+  // Получаем параметры фильтрации из запроса
+  const { date, startDate, endDate } = req.query;
 
-const connection =await mysql.createConnection(CONFIG);
-const query = Q.get_all_apeals;
-const [data] = await connection.execute(query);
+ connection =await mysql.createConnection(CONFIG);
+let query = Q.get_all_appeals;
+let params=[];
+// Фильтрация по конкретной дате
+if (date) {
+  query = Q.get_appeals_by_date;
+  params.push(date);
+}
 
-//console.log(data);
+// Фильтрация по диапазону дат
+if (startDate && endDate) {
+  query = Q.get_appeals_by_between_date;
+  params.push(startDate, endDate);
+}
 
-res.render('all_appeal', { title: 'system-for-handling-requests', appeals: data});
+const [data] = await connection.execute(query, params);
 
+console.log(data);
+
+res.render('all_appeal', {
+  title: 'system-for-handling-requests',
+  appeals: data, // Передаем данные в шаблон
+  currentDate: date, // Передаем текущую дату для отображения в форме
+  currentStartDate: startDate, // Передаем начальную дату для отображения в форме
+  currentEndDate: endDate, // Передаем конечную дату для отображения в форме
 });
+} catch (err) {
+  // Обработка ошибок
+  next(err);
+} finally {
+  // Закрываем соединение, если оно было создано
+  if (connection) {
+    await connection.end();
+  }
+}
+});
+
 module.exports = router;
